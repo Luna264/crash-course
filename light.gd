f6afccab27c3a0938ca20cec59b10a9cf42f432e
Luna264 <ameerawon@gmail.com>
2025-10-20 18:29:01 -0400

    added stoplight function and cars can only be affected by lights if their in a certain area within the light

---
+++ light.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+@onready var green: Sprite2D = $CharacterBody2D/green
+@onready var yellow: Sprite2D = $CharacterBody2D/yellow
+@onready var red: Sprite2D = $CharacterBody2D/red
+var cars_in_area: Array = []
+var color = "red" 
+
+func _ready() -> void:
+	red.visible
+	update_light()
+
+func _on_button_pressed() -> void:
+	match color:
+		"red":
+			color = "yellow"
+		"yellow":
+			color = "green"
+		"green":
+			color = "red"
+	update_light()
+	
+func update_light() -> void:
+	if color == "red":
+		red.visible = true
+		yellow.visible = false
+		green.visible = false
+	if color == "yellow":
+		yellow.visible = true
+		red.visible = false
+		green.visible = false
+	if color == "green":
+		green.visible = true
+		red.visible = false
+		yellow.visible = false
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("cars"):
+		area.get_parent().stoplight = self
+		
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	if area.is_in_group("cars"):
+		area.get_parent().stoplight = null
--- scenes/car.gd
+++ scenes/car.gd
@@ -4,6 +4,7 @@ extends RigidBody2D
 var speed = randi_range(100, 155)
 var knockback_status = false
 var knockback = 200
+var stoplight: Node = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	angular_velocity = 0
@@ -14,19 +15,26 @@ func _process(delta: float) -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
+	if stoplight == null:
+		return
 	var direction = Vector2.UP.rotated(rotation)
 	if knockback_status == false:
 		linear_velocity = speed * direction
-
+	match stoplight.color:
+		"red":
+			speed = 0
+		"yellow":
+			speed = randi_range(50, 70)
+		"green":
+			speed = randi_range(100, 155)
 
 func _on_timer_timeout() -> void:
 	knockback_status = false
 	
-
-
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	knockback_status = true
 	var knockback_dir = (area.global_position - global_position).normalized()
 	if knockback_status == true:
 		apply_central_force(knockback * knockback_dir)
 	timer.start()
+	
